6921d484a905355ba07f2b1eaee06eef6e5e4119
Aldhair Vera <aldhairvera-externo@tgacompany.com>
2024-08-14 20:14:45 -0500

    Form working, inputs working, validation working, error handling working, Show values of a list in a component working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,80 @@
+import { useState } from 'react';
 import './App.css'
 import Card from './components'
 
+const initialState = {
+  name: "",
+  color: "",
+  key: ""
+}
+const initialError = { errorState: false, msg: "" }
+
 function App() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(initialError);
+  const [currentUser, setCurrentUser] = useState(initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    currentUser.key = `${users.length + 1}$`;
+    setUsers([...users, currentUser]);
+    setCurrentUser(initialState);
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentUser({ ...currentUser, [name]: value })
+    validateInput();
+  }
+
+  const validateInput = () => {
+    let formError = false;
+
+    if (currentUser.color.length < 6) {
+      formError = true;
+    }
+
+    if (currentUser.name.startsWith(" ") || currentUser.name.trim().length < 3) {
+      formError = true;
+    }
+
+    if (!formError)
+      setError(initialError);
+    else
+      setError({ errorState: true, msg: "Por favor chequea que la información sea correcta" });
+  }
+
 
   return (
     <div className='App'>
       <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <form className="form" onSubmit={handleSubmit}>
+        <div className='section'>
+          <label htmlFor="name">Nombre Completo:</label>
+          <input type="text" name="name" value={currentUser.name} onChange={handleChange} />
+        </div>
+        <div className='section'>
+          <label htmlFor="color">Ingresa tu color favorito</label>
+          <input type="color" name="color" value={currentUser.color} onChange={handleChange} />
+        </div>
+        <button disabled={error.msg != ""} type="submit">Enviar</button>
+      </form>
+
+      {
+        error.errorState &&
+        <span style={{ color: "red" }}>
+          Por favor chequea la información sea correcta.
+        </span>
+      }
+      {
+        users.length > 0 && (
+          <div className='Cards'>
+            {
+              users.map(user => (<Card key={user.key} name={user.name} color={user.color} />))
+            }
+          </div>
+        )
+      }
     </div>
   )
 }
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { invertColor } from '../utils/OppositeColor';
 
-const Card = () => {
+const Card = ({ name, color }) => {
     return (
-        <div>Hola, soy una tarjeta</div>
+        <div className='card' style={{ borderColor: color }}>
+            <p>Hola {name}</p>
+            <p>
+                Sabemos que tu color favorito es:
+            </p>
+            <div style={{ backgroundColor: color, color: invertColor(color) }} className="ColorBox">
+                {color}
+            </div>
+        </div>
     )
 }
 
